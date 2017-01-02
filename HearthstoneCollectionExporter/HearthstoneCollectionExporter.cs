ea67f20d1387fc5bf323c8fc67a531c80987eed7
Paul Brown <paul90brown@gmail.com>
2017-01-02 05:22:41 -0600

    use HearthDb to get more card info and show missing cards, use Fody

--- HearthstoneCollectionExporter/HearthstoneCollectionExporter.cs
+++ HearthstoneCollectionExporter/HearthstoneCollectionExporter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 using System.IO;
+using CsvHelper;
 using HearthMirror;
-using static HearthMirror.Enums.MirrorStatus;
+using HearthDb;
 
 namespace HearthstoneCollectionExporter
 {
@@ -15,34 +17,69 @@ namespace HearthstoneCollectionExporter
         static void Main()
         {
             var status = HearthMirror.Status.GetStatus().MirrorStatus;
-            if (status == Ok)
+            if (status == HearthMirror.Enums.MirrorStatus.Ok)
             {
                 // Found Hearthstone process, attempting to get cards and write them to CSV.
-                var collection = Reflection.GetCollection();
+                var goldenCollection = Reflection.GetCollection().Where(x => x.Premium);
+                var commonCollection = Reflection.GetCollection().Where(x => !x.Premium);
 
-                using (var w = new StreamWriter("cards.csv"))
+                using (var textWriter = new StreamWriter("cards.csv"))
                 {
-                    var header = "cardId,isGolden,cardCount";
-                    w.WriteLine(header);
+                    var csv = new CsvWriter(textWriter);
 
-                    foreach (var card in collection)
+                    // Write headers
+                    csv.WriteField("Id");
+                    csv.WriteField("Name");
+                    csv.WriteField("Class");
+                    csv.WriteField("Rarity");
+                    csv.WriteField("Mana");
+                    csv.WriteField("Type");
+                    csv.WriteField("Race");
+                    csv.WriteField("Health");
+                    csv.WriteField("Attack");
+                    csv.WriteField("Card Count - Normal");
+                    csv.WriteField("Card Count - Golden");
+                    csv.NextRecord();
+
+                    foreach (var dbCard in Cards.Collectible)
                     {
-                        var cardId = card.Id;
-                        var cardCount = card.Count;
-                        var isGolden = card.Premium;
-                        var newLine = string.Format("{0},{1},{2}", cardId, isGolden, cardCount);
-                        w.WriteLine(newLine);
-                        w.Flush();
+                        csv.WriteField(dbCard.Key); // Id
+                        csv.WriteField(dbCard.Value.Name);
+                        csv.WriteField(dbCard.Value.Class);
+                        csv.WriteField(dbCard.Value.Rarity);
+                        csv.WriteField(dbCard.Value.Cost); // Mana
+                        csv.WriteField(dbCard.Value.Type);
+
+                        if (dbCard.Value.Race == HearthDb.Enums.Race.INVALID)
+                        {
+                            csv.WriteField(null);
+                        }
+                        else
+                        { 
+                            csv.WriteField(dbCard.Value.Race);
+
+                        }
+
+                        csv.WriteField(dbCard.Value.Health);
+                        csv.WriteField(dbCard.Value.Attack);
+
+                        var amountNormal =
+                            commonCollection.Where(x => x.Id.Equals(dbCard.Key)).Select(x => x.Count).FirstOrDefault();
+                        var amountGolden =
+                            goldenCollection.Where(x => x.Id.Equals(dbCard.Key)).Select(x => x.Count).FirstOrDefault();
+                        csv.WriteField(amountNormal);
+                        csv.WriteField(amountGolden);
+
+                        csv.NextRecord();
                     }
                 }
                 MessageBox.Show("Finished exporting cards to cards.csv.", "Success");
-
             }
-            else if (status == ProcNotFound)
+            else if (status == HearthMirror.Enums.MirrorStatus.ProcNotFound)
             {
                 MessageBox.Show("Unable to find Hearthstone the process.", "Error");
             }
-            else if (status == Error)
+            else if (status == HearthMirror.Enums.MirrorStatus.Error)
             {
                 MessageBox.Show("There was a problem finding the Hearthstone process.", "Error");
             }
